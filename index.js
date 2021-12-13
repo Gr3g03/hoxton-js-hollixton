865668041fa3189dee9015de85c15357771da339
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-12-13 12:33:25 +0100

    header + footer html css finished

--- index.js
+++ index.js
@@ -10,20 +10,73 @@ function renderHeader(){
     headerH1El.textContent = 'Hollixton'
 
     const headerNavEl =  document.createElement('nav')
+    headerNavEl.setAttribute('class', 'headerNavEL')
+
     const headerUlEl =  document.createElement('ul')
-    const headerLiEl =  document.createElement('li')
-    headerUlEl.append(headerLiEl)
-    headerNavEl.append(headerUlEl)
+    headerUlEl.setAttribute('class', 'header-ul')
 
-    const leftHaderNavEl =document.createElement('nav')
-    const leftHeaderUlEl = document.createElement('ul')
-    const leftHeaderLiEl = document.createElement('li')
-    leftHaderNavEl.append(leftHeaderUlEl)
-    leftHeaderUlEl.append(leftHeaderLiEl)
+    const girlsHeaderLiEl =  document.createElement('li')
+    girlsHeaderLiEl.setAttribute ('class', 'header-list-item')
+    const girlsALiEl = document.createElement('a')
+    girlsALiEl.setAttribute('href' ,'null')
+    girlsALiEl.textContent = 'Girls'
+    girlsHeaderLiEl.append(girlsALiEl)
 
+    const guysHeaderLiEl =  document.createElement('li')
+    guysHeaderLiEl.setAttribute ('class', 'header-list-item')
+    const gusyALiEl = document.createElement('a')
+    gusyALiEl.setAttribute('href' ,'null')
+    gusyALiEl.textContent = 'Guys'
+    guysHeaderLiEl.append(gusyALiEl)
 
-    headerEL.append(headerH1El,headerNavEl,leftHaderNavEl )
+    const saleHeaderLiEl =  document.createElement('li')
+    saleHeaderLiEl.setAttribute ('class', 'header-list-item')
+    const saleALiEl = document.createElement('a')
+    saleALiEl.setAttribute('href' ,'null')
+    saleALiEl.textContent = 'Sale'
+    saleHeaderLiEl.append(saleALiEl)
+
+    headerUlEl.append(headerH1El, girlsHeaderLiEl, guysHeaderLiEl, saleHeaderLiEl)
+    headerNavEl.append( headerUlEl)
+
+    // const leftHaderSectionEl =document.createElement('section')
+    // leftHaderSectionEl.textContent = 'sale'
+    // const leftHeaderUlEl = document.createElement('ul')
+    // const leftHeaderLiEl = document.createElement('li')
+
+    const rightHeaderSectionEl =  document.createElement('section')
+    rightHeaderSectionEl.setAttribute('class', 'right-HeaderNavEL')
+
+    const rightHeaderrUlEl =  document.createElement('ul')
+    rightHeaderrUlEl.setAttribute('class', 'right-Header-ul')
+
+    const searchLiEL =  document.createElement('li')
+    searchLiEL.setAttribute ('class', 'right-Header-list-item')
+    const searchAEL = document.createElement('a')
+    searchAEL.setAttribute('href' ,'null')
+    searchAEL.textContent = 'search'
+    searchLiEL.append(searchAEL)
+
+    const loginLiEl =  document.createElement('li')
+    loginLiEl.setAttribute ('class', 'right-Header-list-item')
+    const loginALiEl = document.createElement('a')
+    loginALiEl.setAttribute('href' ,'null')
+    loginALiEl.textContent = 'login'
+    loginLiEl.append(loginALiEl)
+
+    const cartLiEl =  document.createElement('li')
+    cartLiEl.setAttribute ('class', 'right-Header-list-item')
+    const cartALiEl = document.createElement('a')
+    cartALiEl.setAttribute('href' ,'null')
+    cartALiEl.textContent = 'cart'
+    cartLiEl.append(cartALiEl)
+
+    rightHeaderrUlEl.append(searchLiEL,loginLiEl,cartLiEl )
+    rightHeaderSectionEl.append(rightHeaderrUlEl)
+    headerEL.append(headerNavEl,rightHeaderSectionEl )
     document.body.append(headerEL)
+
+    
 }
 
 function renderMain(){
